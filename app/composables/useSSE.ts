c4bd97a503e72b3ac487a9afd3da166b2eb349c0
benni.huff@gmail.com <benni.huff@gmail.com>
2026-06-19 11:04:12 +0200

    fix

--- app/composables/useSSE.ts
+++
@@ -1,9 +0,0 @@
-import { SSE } from "sse.js";
-
-export async function useSSE(url: string): Promise<SSE> {
-	const {
-		public: { apiBase },
-	} = useRuntimeConfig();
-
-	return new SSE(`${apiBase}${url}`);
-}
--- server/api/game/[gameId]/sse.ts
+++ server/api/game/[gameId]/sse.ts
@@ -1,11 +1,11 @@
 import { GameEventStream } from "~~/server/utils/sse";
 
 export default defineEventHandler(async (event) => {
-	const gameId = getRouterParam(event, "gameId");
-	if (!gameId) throw createError({ statusCode: 400 });
+  const gameId = getRouterParam(event, "gameId");
+  if (!gameId) throw createError({ statusCode: 400 });
 
-	const eventStream = createEventStream(event);
-	GameEventStream.addStream(gameId, eventStream);
+  const eventStream = createEventStream(event);
+  GameEventStream.addStream(gameId, eventStream);
 
-	return eventStream.send();
+  return eventStream.send();
 });
